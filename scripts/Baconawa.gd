c454fb78d38bcce1760fb73e0f45e3d6c60f64ba
wends05 <cxnner05@gmail.com>
2024-07-03 03:03:58 +0800

    late input control will work

--- scripts/Baconawa.gd
+++ scripts/Baconawa.gd
@@ -1,11 +1,9 @@
 extends Resource
 
 class_name Baconawa
-
-@export var name: String
-
 @export var top_colliding: bool = false
 @export var down_colliding: bool = false
 @export var left_colliding: bool = false
 @export var right_colliding: bool = false
 
+@export var last_input: String
--- scripts/b_controls.gd
+++ scripts/b_controls.gd
@@ -9,6 +9,9 @@ extends CharacterBody2D
 @onready var right_collider = $Colliders/Right
 
 @export var info: Baconawa
+var move = 0
+
+@onready var internal_timer = $InternalTimer
 
 func _ready() -> void:
 	anim.play("idle")
@@ -24,29 +27,63 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("b_down") and not info.down_colliding:
-		velocity = Vector2(0, SPEED)
-		anim.play("move_down")
-	if Input.is_action_pressed("b_up") and not info.top_colliding:
-		velocity = Vector2(0, -SPEED)
-		anim.play("move_up")
-	if Input.is_action_pressed("b_left") and not info.left_colliding:
-		velocity = Vector2(-SPEED, 0)
-		anim.play("move_left")
-	if Input.is_action_pressed("b_right") and not info.right_colliding:
-		velocity = Vector2(SPEED, 0)
-		anim.play("move_right")
-
+	if Input.is_action_pressed("b_down"):
+		if not info.down_colliding:
+			velocity = Vector2(0, SPEED)
+			anim.play("move_down")
+		move = 0
+		info.last_input = "down"
+	if Input.is_action_pressed("b_up"):
+		if not info.top_colliding:
+			velocity = Vector2(0, -SPEED)
+			anim.play("move_up")
+		move = 1
+		info.last_input = "up"
+	if Input.is_action_pressed("b_left"):
+		if not info.left_colliding:
+			velocity = Vector2(-SPEED, 0)
+			anim.play("move_left")
+		move = 2
+		info.last_input = "left"
+	if Input.is_action_pressed("b_right"):
+		if not info.right_colliding:
+			velocity = Vector2(SPEED, 0)
+			anim.play("move_right")
+		move = 3
+		info.last_input = "right"
 	move_and_slide()
 
 func colliding(_body, collider: Area2D, isColliding):
 	print_debug(collider.name, isColliding)
+	print(isColliding, move)
 	match collider.name:
 		"Top":
 			info.top_colliding = isColliding
+			if not isColliding and move == 1:
+				#internal_timer.start(0.5)
+				#await internal_timer
+				velocity = Vector2(0, -SPEED)
+				anim.play("move_up")
 		"Down":
 			info.down_colliding = isColliding
+			if not isColliding and move == 0:
+				#internal_timer.start(0.5)
+				#await internal_timer
+				velocity = Vector2(0, SPEED)
+				anim.play("move_down")
 		"Right":
 			info.right_colliding = isColliding
+			if not isColliding and move == 3:
+				#internal_timer.start(0.5)
+				#await internal_timer
+				velocity = Vector2(SPEED, 0)
+				anim.play("move_right")
 		"Left":
 			info.left_colliding = isColliding
+			if not isColliding and move == 2:
+				#internal_timer.start(0.5)
+				#await internal_timer
+				velocity = Vector2(-SPEED, 0)
+				anim.play("move_left")
+
+	print(velocity.x, " ", velocity.y)
