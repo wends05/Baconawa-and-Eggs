7147590d3be6e3a340c21aa80e31d79cc4eced35
wends05 <cxnner05@gmail.com>
2024-07-04 23:20:52 +0800

    removed resources: i found a way to use the sprites as sort of a class

--- scripts/Baconawa.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-class_name Baconawa
-@export var top_colliding: bool = false
-@export var down_colliding: bool = false
-@export var left_colliding: bool = false
-@export var right_colliding: bool = false
-
-@export var last_input: String
