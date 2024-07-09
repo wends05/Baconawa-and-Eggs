a8aad6046568688ffc41ce53b7bd6b0b6e10dcde
wends05 <cxnner05@gmail.com>
2024-07-09 17:25:38 +0800

    debug texts on screen

--- scripts/game/texts.gd
+++ scripts/game/texts.gd
@@ -2,5 +2,20 @@ extends Label
 
 @export var baconawa: Baconawa
 
-func _process(delta: float) -> void:
-	text = "top:%s\ndown:%s\nleft:%s\nright:%s\n\nlast input:%s" % [baconawa.top_colliding, baconawa.down_colliding, baconawa.left_colliding, baconawa.right_colliding, baconawa.last_input]
+func _process(_delta: float) -> void:
+	text = "top:%s
+	down:%s
+	left:%s
+	right:%s
+
+	last input:%s
+	%s
+	%s" % [
+		baconawa.top_colliding,
+		baconawa.down_colliding,
+		baconawa.left_colliding,
+		baconawa.right_colliding,
+		baconawa.last_input,
+		baconawa.BUFFS[baconawa.buff_id],
+		baconawa.SPEED
+		]
