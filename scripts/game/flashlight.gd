154c0065fcc44a1a704fca36f381930dbe092a71
wends05 <cxnner05@gmail.com>
2024-08-04 14:38:22 +0800

    added logic for when immune / already debuffed on drum instances

--- scripts/game/flashlight.gd
+++ scripts/game/flashlight.gd
@@ -2,14 +2,20 @@ extends Sprite2D
 
 class_name Flashlight
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Baconawa:
+		if body.invincible: return
+		body.debuffed = true
+		if body.state_machine.state.name == "Stunned":
+			return
+		if body.invincible:
+			await body.done_invincible
+		print("Reached this code")
 		body.state_machine.transition_to("Stunned")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Baconawa:
+		if not body.invincible:
+			body.state_machine.transition_to("Stunned")
+
