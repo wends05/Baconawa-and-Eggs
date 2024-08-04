470187367180897250cfa143bf4c61eb075aa99d
wends05 <cxnner05@gmail.com>
2024-08-04 14:34:33 +0800

    Baconawa.debuffed on entering confused/stunned states

--- scripts/states/stunned.gd
+++ scripts/states/stunned.gd
@@ -3,10 +3,14 @@ extends State
 # Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
 # is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(_msg = {}):
+	if not owner is Baconawa:
+		return
+
 	var internal_timer = owner.get_node("InternalTimer")
 	owner.velocity = Vector2(0, 0)
 	owner.anim.play("idle")
 	owner.last_input = ""
 	internal_timer.start(3)
 	await internal_timer.timeout
+	owner.debuffed = false
 	state_machine.transition_to("Idle")
