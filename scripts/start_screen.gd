943481284ef47735816232063fe48a5b51670fc5
Omumuraisu <rycedoesknow@gmail.com>
2024-08-08 12:22:01 +0800

    start screen sounds

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -6,12 +6,14 @@ func _ready():
 	bgmsc.play()
 
 func _on_play_button_up() -> void:
+		sfx.stream = load("res://assets/sfx/sfx_select.wav")
 		sfx.play()
 		await sfx.finished
 		bgmsc.stop()
 		get_tree().change_scene_to_file("res://scenes/screens/instructions_start.tscn")
 
 func _on_instructions_button_up() -> void:
+	sfx.stream = load("res://assets/sfx/sfx_select.wav")
 	sfx.play()
 	await sfx.finished
 	bgmsc.stop()
@@ -19,6 +21,9 @@ func _on_instructions_button_up() -> void:
 
 
 func _on_quit_button_up():
+	sfx.stream = load("res://assets/sfx/sfx_select.wav")
+	sfx.play()
+	await sfx.finished
 	get_tree().quit()
 
 func _on_play_mouse_entered():
