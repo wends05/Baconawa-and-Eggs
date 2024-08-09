5d5b168faa7547d7a69b7eb472afd5b20ccfeb86
Omumuraisu <rycedoesknow@gmail.com>
2024-08-09 21:28:24 +0800

    added end screen

--- scripts/game/results.gd
+++ scripts/game/results.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var label = $VBoxContainer/Label
 @onready var stats = $VBoxContainer/HBoxContainer2/Stats
 @onready var sfx = $AudioStreamPlayer
+@onready var bg = $vic_bg
 
 var transitioning = false
 
@@ -11,9 +12,11 @@ func _ready() -> void:
 	transition.visible = true
 	label.text = "Baconawa won!" if G.moons == 7 else "The Ghosts won!"
 	if label.text == "Baconawa won!":
+		bg.texture = load("res://assets/main_sprites/ui/victory_bacon.png")
 		sfx.stream = load("res://assets/sfx/sfx_busog.wav")
 		sfx.play()
 	else:
+		bg.texture = load("res://assets/main_sprites/ui/victory_rice.png")
 		sfx.stream = load("res://assets/sfx/sfx_gutom.wav")
 		sfx.play()
 	stats.text = "%s moons collected\n%s time remaining" % [G.moons, G.display_time(G.time)]
